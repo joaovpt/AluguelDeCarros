a7030125c43f3ce65678f1327b02697ca4238a4f
joaovpt <joaotassi18@gmail.com>
2025-05-07 19:07:00 -0300

    Commit inicial

---
+++ src/main/java/com/estudos/AluguelDeCarros/AluguelDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.AluguelDeCarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelDeCarrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/AluguelDeCarros/AluguelDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.AluguelDeCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
